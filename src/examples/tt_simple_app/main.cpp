348a51625cad7a9d0ccc7dbd46594599fc04c058
TTropcic <tomislav.tropcic@planetix.hr>
2022-06-28 09:30:33 +0200

    TT_Simple_App

---
+++ src/examples/tt_simple_app/main.cpp
@@ -0,0 +1,11 @@
+#include <px4_platform_common/log.h>
+
+__EXPORT int tt_simple_app_main(int argc, char* argv[]);
+
+int tt_simple_app_main(int argc, char* argv[])
+{
+
+	PX4_INFO("Hello Sky");
+	return OK;
+
+}
